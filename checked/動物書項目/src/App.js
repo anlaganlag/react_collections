1a4311f7b03af5431ae0d9a5f4e3b90805d7dc90
anlaganlag <tanjhu@163.com>
2020-11-26 14:32:10 +0800

    動物書項目實現了搜索書籍

--- checked/動物書項目/src/App.js
+++ checked/動物書項目/src/App.js
@@ -4,22 +4,20 @@ import Grid from "./Grid";
 import { Image, Text } from "./Thumb.styles";
 import Pagination from "./pagination";
 
-
-
 export default function App() {
   //每页展示多少个项目 是2,3,4,5,6的最大公倍数..
   const [perPage, setPerPage] = useState(60);
   //随机展示页码,总项目/每页的ceil即总页数..再根据总页数随机
-  const [page, setPage] = useState(Math.ceil(Math.random() * Math.ceil(lists.length/perPage)));
+  const [page, setPage] = useState(1);
   const [searchQuery, setQuery] = useState("");
 
   // const [curList, setCurList] = useState(
-    // lists.slice(0,perPage)
+  // lists.slice(0,perPage)
   // );
 
   const filterList = lists.filter((book) =>
-  book.title.toLowerCase().includes(searchQuery.toLowerCase())
-);
+    book.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -27,15 +25,13 @@ export default function App() {
     //   lists.slice(perPage * (parseInt(page) - 1), parseInt(page) * perPage)
     // );
     // console.log("当前列表", curList, "提交");
-    setPage(e.target.value)
+    setQuery(e.target.value);
   }
   function handleInput(e) {
-    if (isNaN(e.target.value)) return;
-    setPage(parseInt(e.target.value));
-    
+    setQuery(e.target.value);
   }
   useEffect(() => {
-    console.log("cur", page,typeof page);
+    console.log("cur", page, typeof page);
   }, [page]);
 
   return (
@@ -43,63 +39,59 @@ export default function App() {
       <form onSubmit={handleSubmit}>
         <input
           type={Number}
-          value={page}
+          value={searchQuery}
           onChange={handleInput}
           placeholder="输入页码"
         />
       </form>
       <Pagination
         currentPage={page}
-        setCurrentPage={setPage}  
+        setCurrentPage={setPage}
         pageSize={perPage}
         totalData={filterList.length}
       />
 
       <Grid
         header={
-          page > 0
-            ? `页码 ${page}/${Math.ceil(lists.length / perPage)}`
-            : "动物书封面"
-        }
+          filterList.length > 0 ?
+          `页码 ${page}/${Math.ceil(lists.length / perPage)}`
+        :"404沒有找到書籍"}
       >
-        {filterList.slice((page-1)*perPage,page*perPage).map((item, idx) => (
-          <div className="Thumb">
-            <a href={item.link} target="_blank">
-            <Text>
-              <small>{item.animal}</small>
-            </Text>
+        {filterList
+          .slice((page - 1) * perPage, page * perPage)
+          .map((item, idx) => (
+            <div className="Thumb">
+              <a href={item.link} target="_blank">
+                <Text>
+                  <small>{item.animal}</small>
+                </Text>
 
-            <Image src={item.cover_src} alt="动物图片" />
-            <Text>
-
-              <h5>{item.title}</h5>
-
-            </Text>
-            </a>
-          </div>
-        ))}
+                <Image src={item.cover_src} alt="动物图片" />
+                <Text>
+                  <h5>{item.title}</h5>
+                </Text>
+              </a>
+            </div>
+          ))}
       </Grid>
       <Pagination
         currentPage={page}
-        setCurrentPage={setPage}  
+        setCurrentPage={setPage}
         pageSize={perPage}
         totalData={filterList.length}
       />
-{      page>0 &&
-      <form onSubmit={handleSubmit}>
-        <input
-          type={Number}
-          value={page}
-          onChange={handleInput}
-          placeholder="输入页码"
+      {filterList > 0 && (
+        <form onSubmit={handleSubmit} >
+          <input
+            type={Number}
+            value={page}
+            onChange={handleInput}
+            placeholder="输入页码"
           />
-          <label>
-          {`页码 ${page}/${Math.ceil(lists.length / perPage)}`}
-  
-          </label>
-        <button onClick={()=>setPage(parseInt(page)+1)}>下一页</button>
-
-      </form>}
+          <label>{`页码 ${page}/${Math.ceil(lists.length / perPage)}`}</label>
+          <button onClick={() => setPage(parseInt(page) + 1)}>下一页</button>
+        </form>
+      )}
     </>
   );
 }
