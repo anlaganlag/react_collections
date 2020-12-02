11a9d0d7ee3b73a85b3f91906f9fd0770fdb4550
anlaganlag <tanjhu@163.com>
2020-12-02 14:25:48 +0800

    16項目加入useCallback

--- checked/16分的项目/src/App.js
+++ checked/16分的项目/src/App.js
@@ -1,76 +1,52 @@
-import React, {
-  useState,
-  useEffect,
-  useContext,
-  createContext,
-  useRef,
-} from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import useKeyPress from "./usePressKeyHook";
 import "./App.css";
 
-const CountContext = createContext();
-
-function Counter() {
-  const count = useContext(CountContext);
-  const [test, setTest] = useState(1);
+export default function App() {
+  const [test, setTest] = useState();
   const inputRef = useRef();
+  const roll = useKeyPress("r");
+  function randomNum(seed) {
+    return Math.ceil(Math.random() * seed);
+  }
 
   const resFormat = (idx) =>
     (((idx + 1) * 10000) / 16).toString().split("0")[0];
+  function getRandom() {
+    let newNum = randomNum(15);
+    //如果是偶數和前一個不同時返回
+    if (newNum === +inputRef.current.innerText) {
+      return getRandom();
+    } else {
+      return newNum;
+    }
+  }
+  const rollNum = useCallback(getRandom, [roll]);
 
   useEffect(() => {
-    const getRandom = () => {
-      let newNum = Math.ceil(Math.random() * 2);
-      //如果是偶數和前一個不同時返回
-      if (newNum !== +inputRef.current.innerText) {
-        return newNum;
-      } else {
-        return getRandom();
-      }
-    };
-    const newNum = getRandom();
-    console.log(newNum);
-    setTest(newNum)
-  }, [count]);
+    // const getRandom = () => {
+    //   let newNum = randomNum(15)
+    //   //如果是偶數和前一個不同時返回
+    //   if (newNum === +inputRef.current.innerText) {
+    //     return getRandom();
+    //   } else {
+    //     return newNum;
+    //   }
+    // };
+
+    setTest(rollNum);
+  }, [roll]);
 
   return (
     <>
-      <p>{count}</p>
       <h1 className="q" ref={inputRef}>
         {test}
       </h1>
-      {/* <h2>
-        {count}/16={((count / 16) * 10000).toString().split("0")[0]}
-      </h2> */}
       <p>
         {[...Array(15)].map((i, idx) => {
-          // console.log(idx);
           return <p>{idx + 1 + " " + resFormat(idx)}</p>;
         })}
       </p>
     </>
   );
 }
-
-function App() {
-  const [count, setCount] = useState(1);
-  const addPress = useKeyPress("k");
-  const subPress = useKeyPress("j");
-
-  // console.log(subPress,addPress);
-  return (
-    <div>
-      {/* {addPress && setTimeout(() => dispatch({ type: "add" }), 250)} */}
-
-      <div style={{ display: "none" }}>
-        {subPress && setTimeout(() => setCount(count - 1), 260)}
-        {addPress && setTimeout(() => setCount(count + 1), 260)}
-        hi
-      </div>
-      <CountContext.Provider value={count}>
-        <Counter />
-      </CountContext.Provider>
-    </div>
-  );
-}
-export default App;
