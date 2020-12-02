fbd01f6d41ecefe5ca22795a9b48055f46be9b6a
anlaganlag <tanjhu@163.com>
2020-12-02 13:36:01 +0800

    返回函數可以解決random挑出想要的結果

--- checked/16分的项目/src/App.js
+++ checked/16分的项目/src/App.js
@@ -1,44 +1,51 @@
-import React, { useState, useEffect, useContext, createContext,useRef} from "react";
+import React, {
+  useState,
+  useEffect,
+  useContext,
+  createContext,
+  useRef,
+} from "react";
 import useKeyPress from "./usePressKeyHook";
-import "./App.css"
+import "./App.css";
 
 const CountContext = createContext();
 
 function Counter() {
-  const roll = () =>{
-    return Math.floor( Math.random()*15+1)
-  }
   const count = useContext(CountContext);
-  const [test, setTest] = useState(roll())
-  const inputRef = useRef()
+  const [test, setTest] = useState(1);
+  const inputRef = useRef();
 
+  const resFormat = (idx) =>
+    (((idx + 1) * 10000) / 16).toString().split("0")[0];
 
-  //  (CountContext)/16*10000).toString().split("0")[0])
   useEffect(() => {
-    let newNum = roll()
-    while (newNum % 2 ===0 || newNum ===parseInt(inputRef.current.innerText)){
-      newNum = roll()
-    }
-    console.log(newNum,((newNum) *10000/ 16).toString().split("0")[0]);
-    // console.log(inputRef.current.innerText,"rrrr");
+    const getRandom = () => {
+      let newNum = Math.ceil(Math.random() * 2);
+      //如果是偶數和前一個不同時返回
+      if (newNum !== +inputRef.current.innerText) {
+        return newNum;
+      } else {
+        return getRandom();
+      }
+    };
+    const newNum = getRandom();
+    console.log(newNum);
     setTest(newNum)
-  }, [count])
+  }, [count]);
 
   return (
     <>
-    {/* <p>{count}</p> */}
-      <h1 className="q" ref={inputRef}>{test}</h1>
+      <p>{count}</p>
+      <h1 className="q" ref={inputRef}>
+        {test}
+      </h1>
       {/* <h2>
         {count}/16={((count / 16) * 10000).toString().split("0")[0]}
       </h2> */}
       <p>
         {[...Array(15)].map((i, idx) => {
           // console.log(idx);
-          return (
-            <p>
-              {idx + 1}/16 = {((idx + 1) *10000/ 16).toString().split("0")[0]}
-            </p>
-          );
+          return <p>{idx + 1 + " " + resFormat(idx)}</p>;
         })}
       </p>
     </>
@@ -47,22 +54,9 @@ function Counter() {
 
 function App() {
   const [count, setCount] = useState(1);
-  const subPress = useKeyPress("j");
   const addPress = useKeyPress("k");
-  const resetPress = useKeyPress("m");
+  const subPress = useKeyPress("j");
 
-  const addEightPress = useKeyPress("i");
-  const subEightPress = useKeyPress("u");
-
-  useEffect(() => {
-    resetPress && setCount(1);
-    if (count > 15) {
-      setCount(1);
-    }
-    if (count <= 0) {
-      setCount(15);
-    }
-  }, [resetPress, count]);
   // console.log(subPress,addPress);
   return (
     <div>
@@ -71,8 +65,7 @@ function App() {
       <div style={{ display: "none" }}>
         {subPress && setTimeout(() => setCount(count - 1), 260)}
         {addPress && setTimeout(() => setCount(count + 1), 260)}
-        {addEightPress && setTimeout(() => setCount(count + 8), 260)}
-        {subEightPress && setTimeout(() => setCount(count - 8), 260)}
+        hi
       </div>
       <CountContext.Provider value={count}>
         <Counter />
