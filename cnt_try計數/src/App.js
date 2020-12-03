19bb753c4ca8044bef28cedb7a9c0adddffe12cf
anlaganlag <tanjhu@163.com>
2020-12-03 18:52:23 +0800

    工時第一階段

--- cnt_try計數/src/App.js
+++ cnt_try計數/src/App.js
@@ -6,15 +6,24 @@ export const App = (props) => {
   const [count, setCount] = useState(0);
   const [history, setHistory] = useState([]);
   const [day, setDay] = useState(1);
+  const [date, setDate] = useState(new Date().toLocaleDateString());
   const key1 = "simpleHrs";
   const key2 = "simpleHis";
   const clickRef = useRef();
-  const preCountRef = useRef();
+  const mapper = {
+    0: " 周日 ",
+    1: " 周一 ",
+    2: " 周二 ",
+    3: " 周三 ",
+    4: " 周四 ",
+    5: " 周五 ",
+    6: " 周六 ",
+  };
   function inc(add) {
     console.log(typeof add, typeof count);
-    setCount(+count + add);
+    setCount(+count + parseInt(add));
     const newHis = [
-      `${new Date().toLocaleDateString()}工時:${add}小時`,
+      `${date}${mapper[new Date().getDay()]} 工時(小時) :${add}`,
       ...history,
     ];
     const newCount = +count + add;
@@ -33,26 +42,46 @@ export const App = (props) => {
     const lsHis = localStorage.getItem("simpleHis");
     setHistory(lsHis ? JSON.parse(lsHis) : []);
   }, []);
+  // console.log(preCountRef.current.value)
+  // console.log(preCountRef.current.value,"rrrrr");
 
   return (
     <div>
-      <p ref={preCountRef}>工時: {count}小時 </p>
-      <button onClick={() => setDay(+day + 1)}>+1</button>
-      <button onClick={() => setDay(+day + 5)}>+5</button>
+      <p>本周工時: {count}小時 </p>
+      {/* <button onClick={() => setDay(+day + 1)}>+1</button>
+      <button onClick={() => setDay(+day + 5)}>+5</button> */}
       <form
         onSubmit={(e) => {
           e.preventDefault();
           inc(day);
         }}
       >
+        <p defaultValue={date}>{date}</p>
+
         <input
           value={day}
+          type={Number}
           autoFocus
           ref={clickRef}
           onChange={(e) => setDay(+e.target.value)}
         />
       </form>
-      <button onClick={() => inc(day)}>添加</button>
+      {/* <button onClick={() => inc(day)}>添加</button> */}
+
+      <button
+        onClick={() => {
+          const pre = history[0] ? count - history[0].split(":")[1] : 0;
+          const newHis = history.slice(1) || [];
+          console.log(pre, newHis, "........................");
+
+          setCount(pre);
+          localStorage.setItem("simpleHrs", pre);
+          localStorage.setItem("simpleHis", JSON.stringify(newHis));
+          setHistory(newHis);
+        }}
+      >
+        清除
+      </button>
       <button
         onClick={() => {
           setCount(0);
@@ -63,20 +92,21 @@ export const App = (props) => {
       >
         重置
       </button>
-      {/* <button
+      <button
         onClick={() => {
-          const pre = preCountRef.current.value;
-          const newHis = history.slice(1) || [];
-
-          setCount(pre);
-          localStorage.setItem("simpleHrs", pre);
-          localStorage.setItem("simpleHis", newHis);
-          setHistory(JSON.stringify(newHis));
+          setCount(0);
+          localStorage.setItem("simpleHrs", 0);
+          localStorage.setItem("simpleHis", []);
+          setHistory([]);
         }}
       >
-        清除
-      </button> */}
-      {history.length > 0 && history.map((i, idx) => <p>{i}</p>)}
+        歸檔
+      </button>
+      {history.length > 0 &&
+        history.map((i, idx) => {
+        if (idx<14)
+          {return <p>{i}</p>}
+        })}
     </div>
   );
 };
