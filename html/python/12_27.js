e5d51a5aef1df8dc0b244b0537b6d342657a57bc
anlaganlag <tanjhu@163.com>
2021-01-01 22:25:43 +0800

    熟悉小程序api

--- html/python/12_27.js
+++ html/python/12_27.js
@@ -1,4 +1,21 @@
-const Foo = class NamedFoo {
-    whoIsThere:NamedFoo.name;
-  }
-console.log(typeof Foo);
+const h = () => {}
+
+
+
+
+
+//执行时 绑定了h和0...
+//g是内部的生成的会被销毁..
+//即首次执行时记住了h和x=0
+const f = (fn,x) => {
+    const g = () => console.log(x)
+    if (x<1){
+        f(g,1)
+    }else {
+        fn()
+    }
+    
+}
+f(h,0)
+
+// console.log(f(h,0))
