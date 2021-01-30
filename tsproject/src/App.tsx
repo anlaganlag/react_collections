aeabb694ccd048c36c7ed5527e1c842d7774afc3
anlaganlag <tanjhu@163.com>
2021-01-30 23:12:48 +0800

    新react项目

---
+++ tsproject/src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import MainPage from "./Pages/MainPage/MainPage";
+import SignInPage from "./Pages/SignInPage/SignInPage";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <div className="top">
+        <Navbar />
+      </div>
+      <Router>
+        <Switch>
+          <Route path="/" exact render={() => <MainPage />} />
+          <Route path="/signin" exact render={() => <SignInPage />} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
