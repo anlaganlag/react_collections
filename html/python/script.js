207e3e6549527f2f2e83c46229b2a81530c14746
anlaganlag <tanjhu@163.com>
2021-02-18 16:31:00 +0800

    emit父组建绑定事件子组件emit

--- html/python/script.js
+++ html/python/script.js
@@ -1,10 +1,13 @@
 Vue.component("blog-post", {
   props: ["post"],
   template: `
-    <div class="blog-post">
-      <h3>{{ post.title }}</h3>
-      <div v-html="post.content"></div>
-    </div>
+  <div class="blog-post">
+  <h3>{{ post.title }}</h3>
+  <button v-on:click="$emit('enlarge-text')">
+    Enlarge text
+  </button>
+  <div v-html="post.content"></div>
+</div>
   `,
 });
 
@@ -12,9 +15,10 @@ new Vue({
   el: "#blog-post-demo",
   data: {
     posts: [
-      { id: 1, title: "一.My journey with Vue" ,content:"最强Pro"},
-      { id: 2, title: "二.Blogging with Vue",content:"天选之人theOne" },
-      { id: 3, title: "三.Why Vue is so fun",content:"价值" },
+      { id: 1, title: "一.My journey with Vue", content: "最强Pro" },
+      { id: 2, title: "二.Blogging with Vue", content: "天选之人theOne" },
+      { id: 3, title: "三.Why Vue is so fun", content: "价值" },
     ],
+    postFontSize: 1,
   },
 });
