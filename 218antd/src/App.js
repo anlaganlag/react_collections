4a2699cddf865987bd528a5369c9b5ce51bb80e6
anlaganlag <tanjhu@163.com>
2021-02-20 16:55:16 +0800

    提问项目

---
+++ 218antd/src/App.js
@@ -0,0 +1,12 @@
+import { DatePicker } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <DatePicker />
+
+    </div>
+  );
+}
+
+export default App;
--- html/python/script.js
+++ html/python/script.js
@@ -1,24 +1,39 @@
-Vue.component("blog-post", {
-  props: ["post"],
-  template: `
-  <div class="blog-post">
-  <h3>{{ post.title }}</h3>
-  <button v-on:click="$emit('enlarge-text')">
-    Enlarge text
-  </button>
-  <div v-html="post.content"></div>
-</div>
-  `,
-});
-
-new Vue({
-  el: "#blog-post-demo",
+var watchExampleVM = new Vue({
+  el: '#watch-example',
   data: {
-    posts: [
-      { id: 1, title: "一.My journey with Vue", content: "最强Pro" },
-      { id: 2, title: "二.Blogging with Vue", content: "天选之人theOne" },
-      { id: 3, title: "三.Why Vue is so fun", content: "价值" },
-    ],
-    postFontSize: 1,
+    question: '',
+    answer: 'I cannot give you an answer until you ask a question!'
   },
-});
+  watch: {
+    // 如果 `question` 发生改变，这个函数就会运行
+    question: function (newQuestion, oldQuestion) {
+      this.answer = 'Waiting for you to stop typing...'
+      this.debouncedGetAnswer()
+    }
+  },
+  created: function () {
+    // `_.debounce` 是一个通过 Lodash 限制操作频率的函数。
+    // 在这个例子中，我们希望限制访问 yesno.wtf/api 的频率
+    // AJAX 请求直到用户输入完毕才会发出。想要了解更多关于
+    // `_.debounce` 函数 (及其近亲 `_.throttle`) 的知识，
+    // 请参考：https://lodash.com/docs#debounce
+    this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+  },
+  methods: {
+    getAnswer: function () {
+      if (this.question.indexOf('?') === -1) {
+        this.answer = 'Questions usually contain a question mark. ;-)'
+        return
+      }
+      this.answer = 'Thinking...'
+      var vm = this
+      axios.get('https://yesno.wtf/api')
+        .then(function (response) {
+          vm.answer = _.capitalize(response.data.answer)
+        })
+        .catch(function (error) {
+          vm.answer = 'Error! Could not reach the API. ' + error
+        })
+    }
+  }
+})
